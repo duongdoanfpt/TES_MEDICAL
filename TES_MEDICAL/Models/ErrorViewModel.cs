f019d5679bcf78f7f33bd5425cdd85a738ec0f0c
ThanhDuyITC <duydt.itc@gmail.com>
2021-09-12 16:46:29 +0700

    Add project files.

---
+++ TES_MEDICAL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TES_MEDICAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
