649c92dd933fcc0182ae0656191be32c851ae968
duongdoanfpt <duongdtbps11905@fpt.edu.vn>
2021-09-12 17:10:47 +0700

    Create Benhnhan.cs
    Add Class Benh Nhan

---
+++ TES_MEDICAL/Models/Benhnhan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TES_MEDICAL.Models
+{
+    public class Benhnhan
+    {
+        public int BenhnhanID { get; set; }
+
+    }
+}
