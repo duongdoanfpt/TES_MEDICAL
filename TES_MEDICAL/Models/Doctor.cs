4beb646268e92598d4b027222ae80d5a400d0cee
duydt.itc <duydt.itc@gmail.com>
2021-09-13 09:47:56 +0700

    ComitTest

---
+++ TES_MEDICAL/Models/Doctor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TES_MEDICAL.Models
+{
+    public class Doctor
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+    }
+}
